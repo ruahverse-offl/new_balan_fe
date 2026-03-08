fbeeeb90ffe830b325efc6b02a0fde8f2c3efba9
ruahverse-offl <ruahverse@gmail.com>
2026-03-08 16:16:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: [
+      "newbalanpharmacy.com",
+      "localhost"
+    ]
+  }
 })
